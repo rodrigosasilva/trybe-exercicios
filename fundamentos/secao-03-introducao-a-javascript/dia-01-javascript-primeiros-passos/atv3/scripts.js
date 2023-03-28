96606b402d826fcd4329eacef174c19e2cba7a86
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-03-28 15:33:18 -0300

    Condições if e else

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/atv3/scripts.js
@@ -0,0 +1,11 @@
+let nota = 95;
+
+if (nota >= 80 && nota <=100) {
+    console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!");
+}
+else if (nota >= 60 && nota < 80) {
+    console.log("Você está na nossa lista de espera.");
+}
+else if (nota < 60) {
+    console.log("Infelizmente, você reprovou.");
+}
