b885a2f7f71707e5a9b7ceb9389715445ac6cd51
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-03-28 17:28:18 -0300

    Quesito 1 ao 4

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio/scripts.js
@@ -0,0 +1,47 @@
+let a = 10;
+let b = 7;
+let c = 14;
+/*
+//Quesito 1
+console.log(a + b); //soma
+console.log(a - b); //subtração
+console.log(a * b); //multipicação
+console.log(a / b); //divisão
+console.log(a % b); //sobra da divião
+
+//Quesito 2
+if (a > b){
+    console.log(a);
+}
+else if (a < b){
+    console.log(b);
+}
+else {
+    console.log("São iguais!");
+}
+
+//Quesito 3
+if (a > b && a > c){
+    console.log(a);
+}
+else if (a < b && b > c){
+    console.log(b);
+}
+else if (a < c && c > b){
+    console.log(c);
+}
+
+//Quesito 4
+let n = 1;
+
+if (n > 0){
+    console.log("positive");
+}
+else if (n < 0){
+    console.log("negative");
+}
+else {
+    console.log("zero");
+}
+*/
+//Quesito 5
