584c1ad4890eafb8805d8ca2f000a7b09c36a7d7
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-04-03 16:21:43 -0300

    Exercícios – objetos e for/in

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/execise1.js
@@ -0,0 +1,41 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+//1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo o nome dela. Para isso, utilize a sintaxe meuObjeto.chave. Então, o valor esperado no console é:
+
+console.log('Bem vinda, ' + info.personagem);
+
+//2 - Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Para isso, use a sintaxe meuObjeto['chave'] = valor. Então, o valor esperado no console é:
+
+info.recorrente = 'Sim';
+console.log(info);
+
+//3 - Faça um for/in que mostre todas as chaves do objeto. O valor esperado no console é:
+
+for (let key in info) {
+  console.log(key);
+}
+
+//4 - Faça um novo for/in, mas agora mostre todos os valores das propriedades do objeto. O valor esperado no console é:
+
+
+for (let value in info) {
+  console.log(info[value]);
+}
+
+//5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: “Tio Patinhas”, “Christmas on Bear Mountain, Dell’s Four Color Comics #178”, “O último MacPatinhas”, “Sim”. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. O valor esperado no console é:
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let key in info) {
+  console.log((info[key]) + ' e ' + (info2[key]));
+}
+
--- fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/objetos.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/objetos.js
@@ -6,9 +6,9 @@
 //let medals = { golden: 2, silver: 3 };
 
 let player = {
-    name : 'Marta',
-    lastName : 'Silva',
-    age : 34,
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
     medals: {
         golden: 2,
         silver: 3,
@@ -17,7 +17,7 @@ let player = {
 
 //Acesse as chaves name, lastName e age por meio da sintaxe meuObjeto.chave e concatene as informações para imprimir no console uma mensagem no seguinte formato: “A jogadora Marta Silva tem 34 anos de idade”.
 
-console.log('A jogadora '+player.name +' '+player.lastName+' tem '+player.age+' anos de idade.');
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
 
 //Adicione ao objeto a chave bestInTheWorld por meio da sintaxe meuObjeto.chave = valor e atribua a essa chave um array que contenha as datas em que a jogadora Marta foi considerada a melhor do mundo.
 
@@ -25,7 +25,7 @@ player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 
 //Acesse a chave bestInTheWorld por meio da sintaxe meuObjeto['chave'] e faça um console.log no seguinte formato: “A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes”.
 
-console.log('A jogadora '+player.name +' '+player.lastName+' foi eleita melhor do mundo por '+player.bestInTheWorld.length+' vezes.')
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.')
 
 //Acesse a chave medals por meio da sintaxe meuObjeto.chave e faça um console.log no seguinte formato: “A jogadora possui 2 medalhas de ouro e 3 medalhas de prata”.
-console.log('A jogadora possui '+player.medals.golden+' medalhas de ouro e '+player.medals.golden+' medalhas de prata.');
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.golden + ' medalhas de prata.');
