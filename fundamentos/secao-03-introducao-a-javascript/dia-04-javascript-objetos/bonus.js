bbdd39f426725ebfe7ee67d1342800bb11e5cd1c
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-04-04 23:10:41 -0300

    Exercicios bonus

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/bonus.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/bonus.js
@@ -58,3 +58,34 @@ let allLessons = Object.assign({} , {lesson1, lesson2, lesson3});
 //console.log(allLessons);
 
 //Com base no objeto elaborado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas.
+function getNumberOfStudents(obj) {
+  let total = 0;
+
+  let keys = Object.keys(obj);
+    
+  for (index in keys) {
+    total += obj[keys[index]].numeroEstudantes; // incremente a variável total a cada iteração
+  }
+  return total; // a função retorna o total de estudantes após as iterações do "for/in"
+}
+// console.log(getNumberOfStudents(allLessons));
+
+//Crie uma função que obtenha o valor da chave de acordo com sua posição no objeto. Por exemplo:
+function getValueByNumber(obj, number) {
+  return Object.values(obj)[number];
+}
+// usa-se o "Object.values()" para criar um array com os valores do objeto e, então, pode-se acessar sua posição passando o índice.
+
+console.log(getValueByNumber(lesson1, 2));
+
+//Crie uma função que verifique se o par chave/valor existe na função. Essa função deve ter três parâmetros: o objeto, o nome da chave e o valor dela.
+
+function verifyPair(obj, key, value) {
+  let entries = Object.entries(obj);
+  let isEqual = false;
+  for (let index in entries) {
+    if (entries[index][0] === key && entries[index][1] === value) isEqual = true;
+  }
+  return isEqual;
+}
+console.log(verifyPair(lesson2,'professor','Carlos'));
