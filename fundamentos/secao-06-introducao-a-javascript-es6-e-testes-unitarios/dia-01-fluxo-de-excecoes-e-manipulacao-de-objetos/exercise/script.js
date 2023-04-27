62735173fb488edd675210bc856bdfdf0d8f0358
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-04-27 18:35:33 -0300

    Parte III - Trabalhando em uma autoescola

--- fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/exercise/script.js
+++ fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/exercise/script.js
@@ -147,3 +147,27 @@ const findPeopleByState = (state) => {
     return error.message;
   }
 };
+
+const validateInfo = (name, age) => {
+  if (!name || !age) {
+    throw new Error("Todas as informações são necessárias");
+  }
+};
+
+const validateLegalAge = (age) => {
+  if (age < 18) {
+    throw new Error(
+      "Ops, infelizmente nesse momento você não pode fazer as aulas"
+    );
+  }
+};
+
+const studentRegister = (name, age) => {
+  try {
+    validateInfo(name, age);
+    validateLegalAge(age);
+    return `${name}, boas-vindas à AuTrybe!`;
+  } catch (error) {
+    return error.message;
+  }
+};
