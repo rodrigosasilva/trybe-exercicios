aea7c170fb82cee73ea1e92177b2373bd8e09a4b
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-05-08 15:01:31 -0300

    Bonus parte II completo

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/bonus.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/bonus.js
@@ -60,3 +60,37 @@ const mageAttack = (mage) => {
   }
   return turnStats;
 };
+
+
+// Crie a primeira HOF que compõe o objeto gameActions.
+// Crie a segunda HOF que compõe o objeto gameActions.
+// Crie a terceira HOF que compõe o objeto gameActions.
+// Adicione ao objeto gameActions uma função de primeira classe que retorne o objeto battleMembers atualizado.
+
+
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const warriorDamage = warriorAttack(warrior);
+    warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
+  },
+  mageTurn: (mageAttack) => {
+    const mageTurnStats = mageAttack(mage);
+    const mageDamage = mageTurnStats.damageDealt;
+    mage.damage = mageDamage;
+    mage.mana -= mageTurnStats.manaSpent;
+    dragon.healthPoints -= mageDamage;
+  },
+  dragonTurn: (dragonAttack) => {
+    const dragonDamage = dragonAttack(dragon);
+    mage.healthPoints -= dragonDamage;
+    warrior.healthPoints -= dragonDamage;
+    dragon.damage = dragonDamage;
+  },
+  turnResults: () => battleMembers,
+};
+
+gameActions.warriorTurn(warriorAttack);
+gameActions.mageTurn(mageAttack);
+gameActions.dragonTurn(dragonAttack);
+console.log(gameActions.turnResults());
