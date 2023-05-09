424bf5dd0925cf096c9d1c3f27c9980d48226bef
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-05-09 16:00:08 -0300

    Finalizando exercicio

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercise.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercise.js
@@ -96,3 +96,20 @@ const countLetterA = countries
   }, 0);
 
 console.log(countLetterA);
+
+// 🚀 5 - Crie um array de objetos e calcule a média de notas. Utilize as constantes students e grades para criar um array de objetos e calcule a média da nota das pessoas estudantes
+
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const studentAverage = () => {
+  return students.map((student, index) => (
+    {
+      name: student,
+      average: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+    }
+    ));
+}
+
+console.log(studentAverage(students, grades));
