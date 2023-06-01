b576a31e89f2ad932f45c8d7454f39b9118b9601
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-05-31 21:46:24 -0300

    Configuração, Title e ModuleDetails

---
+++ front-end/secao-02-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/react-with-vite/vite-project/src/components/ModuleDetails.tsx
@@ -0,0 +1,12 @@
+function ModuleDetails() {
+  return(
+    <div>
+      <h2>Fundamentos</h2>
+      <h2>Front-end</h2>
+      <h2>Back-end</h2>
+      <h2>Ciência da computação</h2>
+    </div>
+  );
+}
+
+export default ModuleDetails;
