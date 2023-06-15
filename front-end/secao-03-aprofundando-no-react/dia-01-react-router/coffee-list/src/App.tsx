757891cd0f0339de287e21478bb8905696c32c58
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-06-15 17:00:46 -0300

    implementando outlet

--- front-end/secao-03-aprofundando-no-react/dia-01-react-router/coffee-list/src/App.tsx
+++ front-end/secao-03-aprofundando-no-react/dia-01-react-router/coffee-list/src/App.tsx
@@ -3,17 +3,18 @@ import './App.css';
 import Home from './pages/home';
 import CoffeeList from './pages/coffee-list';
 import Coffee from './pages/coffee';
-import NavBar from './components/nav-bar';
 import NotFound from './pages/not-found';
+import Layout from './components/layout';
 
 function App() {
   return (
     <>
-      <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/coffees" element={<CoffeeList />} />
-        <Route path="/coffees/:coffee" element={ <Coffee /> } />
+        <Route path="/" element={ <Layout /> }>
+          <Route index element={ <Home /> } />
+          <Route path="coffees" element={ <CoffeeList /> } />
+          <Route path="coffees/:coffee" element={ <Coffee /> } />
+        </Route>
         <Route path="/*" element={ <NotFound /> } />
       </Routes>
     </>
