fe73a33d7657f5123594a322cb55e63d73fe9e36
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-06-15 15:56:00 -0300

    implementando navegação - Link e useNavigate

--- front-end/secao-03-aprofundando-no-react/dia-01-react-router/coffee-list/src/pages/coffee-list/index.tsx
+++ front-end/secao-03-aprofundando-no-react/dia-01-react-router/coffee-list/src/pages/coffee-list/index.tsx
@@ -1,6 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
 function CoffeeList() {
+
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/');
+  }
+
   return (
-    <h1>CoffeeList page</h1>
+    <>
+      <h1>CoffeeList page</h1>
+      <button onClick={ handleClick }>
+        Página inicial
+      </button>
+    </>
+
   )
 }
 
