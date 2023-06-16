ea60e03c263d82a55924383234f39f6a8d8905e2
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-06-16 17:05:36 -0300

    implementando useEffect com função assíncona

--- front-end/secao-03-aprofundando-no-react/dia-02-useeffect/international-space-statio-location/src/App.tsx
+++ front-end/secao-03-aprofundando-no-react/dia-02-useeffect/international-space-statio-location/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { fetchCoordinates } from './service';
+
+type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCoordinates();
+      setCoordinates({ latitude: data.latitude, longitude: data.longitude });
+    }
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <h1>International Space Station Location Tracker</h1>
+  );
 }
 
-export default App
+export default App;
